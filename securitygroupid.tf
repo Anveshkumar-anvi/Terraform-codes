e4fe966cff3a672c0a299219b1dc5d30c91ec816
Anveshkumar <105491595+Anveshkumar-anvi@users.noreply.github.com>
2023-08-03 10:00:04 +0000

    Update securitygroupid.tf
    written inbound & outbound rules in the code

--- securitygroupid.tf
+++ securitygroupid.tf
@@ -4,13 +4,18 @@ resource "aws_security_group" "sg" {
   description = "Terraform-SG-Training"
   vpc_id      = "vpc-058ca6cc330d77e69"
 
+
+******* ingress means inbound rule ***********
+
   ingress {
     from_port        = 0
     to_port          = 0
     protocol         = "All"
     cidr_blocks      = ["0.0.0.0/0"]
   }
-  
+
+***** egress means outbound rule ************
+
   egress {
     from_port        = 0
     to_port          = 0
