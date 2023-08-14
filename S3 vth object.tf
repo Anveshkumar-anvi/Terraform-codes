60c3eba4602640cc6a4f3bb9937921b58d724809
Anveshkumar <105491595+Anveshkumar-anvi@users.noreply.github.com>
2023-08-14 08:42:42 -0400

    Update S3 vth object.tf
    bucket name edited

--- S3 vth object.tf
+++ S3 vth object.tf
@@ -1,5 +1,5 @@
-resource "aws_s3_bucket" "s3-bucket1121" {
-  bucket = "s3-bucket1121"
+resource "aws_s3_bucket" "s3-bucket13142" {
+  bucket = "s3-bucket13142"
 
   tags = {
     Name        = "s3-bucket1"
@@ -8,7 +8,7 @@ resource "aws_s3_bucket" "s3-bucket1121" {
 }
 
 resource "aws_s3_object" "object" {
-  bucket = "s3-bucket1121"
+  bucket = "s3-bucket13142"
   key    = "object-131"
  
   
