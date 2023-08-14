88d4164d7ec2a420f0b897fe4090a81eb1124a2c
Anveshkumar <105491595+Anveshkumar-anvi@users.noreply.github.com>
2023-08-14 09:00:06 -0400

    Update S3 vth object.tf

--- S3 vth object.tf
+++ S3 vth object.tf
@@ -1,5 +1,5 @@
-resource "aws_s3_bucket" "s3-bucket13142" {
-  bucket = "s3-bucket13142"
+resource "aws_s3_bucket" "s3-bucket100112" {
+  bucket = "s3-bucket100112"
 
   tags = {
     Name        = "s3-bucket1"
@@ -8,7 +8,7 @@ resource "aws_s3_bucket" "s3-bucket13142" {
 }
 
 resource "aws_s3_object" "object" {
-  bucket = "s3-bucket13142"
+  bucket = "s3-bucket100112"
   key    = "object-131"
  
   
