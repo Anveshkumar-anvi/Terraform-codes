1f17aa512701cbc4e44392d2c1c1ab121e9ed34a
Anveshkumar <105491595+Anveshkumar-anvi@users.noreply.github.com>
2023-08-14 08:59:22 -0400

    Update securitygroupid.tf
    removed ingrees bound and outbound names in code

--- securitygroupid.tf
+++ securitygroupid.tf
@@ -4,9 +4,6 @@ resource "aws_security_group" "sg" {
   description = "Terraform-SG-Training"
   vpc_id      = "vpc-058ca6cc330d77e69"
 
-
-******* ingress means inbound rule ***********
-
   ingress {
     from_port        = 0
     to_port          = 0
@@ -14,7 +11,6 @@ resource "aws_security_group" "sg" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
-***** egress means outbound rule ************
 
   egress {
     from_port        = 0
