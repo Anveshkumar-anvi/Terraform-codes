403963945f29bcb9c35ef0848b76e9100585cbee
Anveshkumar <105491595+Anveshkumar-anvi@users.noreply.github.com>
2023-08-15 02:58:28 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
   access_key = "AKIA6PL5QY4JSAUCY2M2"
-  secret_key = "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
-"
+  secret_key = "czo+uh9IfKg1MEM5Vcds0XURlcUzjQrjOubZTsOG"
 }
