15533e237d4594c8d3583b317c55a95534db7a92
Anveshkumar <105491595+Anveshkumar-anvi@users.noreply.github.com>
2023-08-16 03:34:52 -0400

    Update provider.tf
    access key changed

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIA6PL5QY4JSAUCY2M2"
-  secret_key = "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"
+  access_key = "AKIA3SCL7LLEQGXDAE7H"
+  secret_key = "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"
 }
